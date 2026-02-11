d90e49579db3c5b5bd184929d93c517f0b1fe1d4
natbediee <natbediee@gmail.com>
2026-02-11 16:05:45 +0100

    html

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-import uvicorn 
-if __name__ == "__main__": 
-    uvicorn.run("api:app", host="127.0.0.1", port=8000)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
